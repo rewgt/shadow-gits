f7bedb1bdef0c38e42796132b52839ee00f94752
wayne chan <shadow_widget@126.com>
2017-05-29 18:00:05 +0800

    17-05-29

--- shadow-gits.js
+++ shadow-gits.js
@@ -2,6 +2,62 @@
 
 if (!window.W) { window.W = new Array(); W.$modules = [];} W.$modules.push( function(require,module,exports) {
 
+if (!Array.prototype.findIndex) {  // polyfill function
+  Array.prototype.findIndex = function(predicate) {
+    // 1. Let O be ? ToObject(this value).
+    if (this == null) {
+      throw new TypeError('"this" is null or not defined');
+    }
+
+    var o = Object(this);
+
+    // 2. Let len be ? ToLength(? Get(O, "length")).
+    var len = o.length >>> 0;
+
+    // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+
+    // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+    var thisArg = arguments[1];
+
+    // 5. Let k be 0.
+    var k = 0;
+
+    // 6. Repeat, while k < len
+    while (k < len) {
+      // a. Let Pk be ! ToString(k).
+      // b. Let kValue be ? Get(O, Pk).
+      // c. Let testResult be ToBoolean.
+      // d. If testResult is true, return k.
+      var kValue = o[k];
+      if (predicate.call(thisArg, kValue, k, o)) {
+        return k;
+      }
+      // e. Increase k by 1.
+      k++;
+    }
+
+    // 7. Return -1.
+    return -1;
+  };
+}
+
+if (!Array.prototype.find) {  // polyfill function
+  var fn_array_find_ = Array.prototype.findIndex;
+  Array.prototype.find = function() {
+    // 1. Let O be ? ToObject(this value).
+    if (this == null) {
+      throw new TypeError('"this" is null or not defined');
+    }
+
+    var o = Object(this);
+    var i = fn_array_find_.apply(o,arguments);
+    return i >= 0? o[i]: undefined;
+  };
+}
+
 var W = require('shadow-widget');
 var utils = W.$utils;
 
@@ -14,7 +70,7 @@ var Local  = {};
 utils.gitOf = function(sDomain,sProtocol) {
   if (!sDomain || typeof sDomain != 'string')
     return Git;
-  else if (sDomain.startsWith('localhost') || sDomain.startsWith('127.0.0.1')) {
+  else if (sDomain.indexOf('localhost') == 0 || sDomain.indexOf('127.0.0.1') == 0) {
     Local.siteDomain = sDomain;
     if (sProtocol) Local.siteProtocol = sProtocol;
     return Local;
